03eea0297bf5017c8944c80594af743a972d5f47
andyexeter <palmer.andy@gmail.com>
2016-11-04 09:27:31 +0000

    Update dependencies

--- examples/curl.php
+++ examples/curl.php
@@ -4,7 +4,7 @@ use Palmtree\Http\Curl;
 
 require dirname( __DIR__ ) . '/vendor/autoload.php';
 
-$curl = new Curl( 'http://www.google.co.uk' );
+$curl = new Curl( 'https://www.google.co.uk' );
 
 var_dump( $curl->getHeaders() );
 var_dump( $curl->getHttpStatus() );
